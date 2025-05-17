2df04cacaa59377a40a76e6c4a6705cc1f2f8efd
Maksum <maksum.zein99@gmail.com>
2025-05-17 12:28:02 +0700

    Add files via upload

---
+++ Latihan1.py
@@ -0,0 +1,55 @@
+#1. Nyapa Dunia yang Penuh Sampah (Super Gampang)
+print("#1. Halo Dunia! Ayo kurangi sampah plastik bareng Maksum!")
+
+#2. Pake Tanda Kutip Tunggal (Masih Gampang)
+print('#2. Halo Dunia! Matikan lampu kalo ga dipake, saran Maksum!')
+
+#3. Pake Variabel buat Pesan Lingkungan (Gampang)
+pesan = "#3. Halo Dunia! Ayo tanam pohon bareng Maksum!"
+print(pesan)
+
+#4. Gabungin Dua Teks (Masih Gampang)
+print("#4. Halo Dunia! " + "Ayo bantu anak putus sekolah bareng Maksum!")
+
+#5. Pake f-String (Mulai Agak Menantang)
+nama = "Maksum"
+pesan = "#5. Halo Dunia! Ayo dukung UMKM lokal bareng"
+print(f"{pesan} {nama}!")
+
+#6. Pake .format() (Agak Menantang)
+print("#6. Halo Dunia! {} kurangi polusi udara bareng {}!".format("Ayo", "Maksum"))
+
+#7. Cetak Berulang (Agak Menantang)
+print("#7. Ayo hemat air bareng Maksum! " * 3)
+
+#8. Pake Escape Character (Menantang)
+print("#8. Halo Dunia!\nAyo donasi buku bareng Maksum!")
+
+#9. Pake End Parameter (Menantang)
+print("#9. Halo Dunia! Ayo beli produk lokal ", end="")
+print("bareng Maksum!")
+
+#10. Pake Separator (Menantang)
+print("#10. Halo Dunia", "Ayo daur ulang sampah bareng Maksum!", sep=" - ")
+
+#11. Kombinasi f-String dan Separator (Agak Sulit)
+nama = "Maksum"
+kegiatan = "kurangi emisi karbon"
+print(f"#11. Halo Dunia, {kegiatan}", f"bareng {nama}!", sep=" => ")
+
+#12. Pake Escape Character dan End (Sulit)
+print("#12. Halo Dunia!\nAyo hemat listrik ", end="")
+print("bareng Maksum!")
+
+#13. Gabungin f-String dan Ulang (Sulit)
+nama = "Maksum"
+print(f"#13. Ayo donasi buku bareng {nama}! " * 2)
+
+#14. Kombinasi .format() dan Escape (Lebih Sulit)
+print("#14. Halo Dunia!\n{} dukung UMKM bareng {}!".format("Ayo", "Maksum"))
+
+#15. Semua Digabung (Paling Sulit)
+nama = "Maksum"
+kegiatan = "daur ulang sampah"
+print(f"#15. Halo Dunia!\n{kegiatan} ", f"bareng {nama} " * 2, sep=" => ", end="")
+print("yuk!")
